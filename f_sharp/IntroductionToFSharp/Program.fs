0620c4dd4774f5cad2e3203539b39d8878a7d218
ZaiiiRan <a1b075753@gmail.com>
2025-03-18 13:29:29 +0300

    cylinder volume

--- f_sharp/IntroductionToFSharp/Program.fs
+++ f_sharp/IntroductionToFSharp/Program.fs
@@ -14,6 +14,12 @@ let solve a b c =
         if D<0. then None
         else Quadratic(((-b+sqrt(D))/(2.0*a), (-b-sqrt(D))/(2.0*a)))
 
+let circleArea r = (Math.PI*r*r)
+let cylinderVolume r h = (circleArea r) * h
+
+let multiplyAreaH area h = area * h
+let cylinderVolumeSuperPosition = (circleArea >> multiplyAreaH)
+
 [<EntryPoint>]
 let main (argv :string[]) =
     printfn "Hello World"
@@ -29,5 +35,25 @@ let main (argv :string[]) =
 
     Console.WriteLine("---------------------")
 
-    
+    Console.WriteLine("Введите радиус:")
+    let r = System.Console.ReadLine() |> float
+    Console.WriteLine("Введите высоту:")
+    let h = System.Console.ReadLine() |> float
+
+    let volume = cylinderVolume r h
+    printfn "Объем цилиндра: %f" volume
+
+    Console.WriteLine("---------------------")
+
+    Console.WriteLine("Введите радиус:")
+    let r2 = System.Console.ReadLine() |> float
+    Console.WriteLine("Введите высоту:")
+    let h2 = System.Console.ReadLine() |> float
+
+    let volume2 = cylinderVolumeSuperPosition r h
+    printfn "Объем цилиндра: %f" volume2
+
+    Console.WriteLine("---------------------")
+
+
     0
