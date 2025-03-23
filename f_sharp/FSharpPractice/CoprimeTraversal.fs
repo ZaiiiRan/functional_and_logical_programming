95d0dbc15956e0bb15a0be0e71c5c3360f466a42
ZaiiiRan <a1b075753@gmail.com>
2025-03-23 23:07:35 +0300

    5.13: coprime traversal

---
+++ f_sharp/FSharpPractice/CoprimeTraversal.fs
@@ -0,0 +1,14 @@
+module CoprimeTraversal
+
+let rec gcd x y =
+    match y with
+    | 0 -> x
+    | _ -> gcd y (x % y)
+
+let coprimeTraversal number (func :int->int->int) initial =
+    let rec traversal number (func :int->int->int) acc candidate =
+        if candidate <= 0 then acc
+        else
+            let newAcc = if gcd number candidate = 1 then (func acc candidate) else acc
+            traversal number func newAcc (candidate-1)
+    traversal number func initial number
--- f_sharp/FSharpPractice/Program.fs
+++ f_sharp/FSharpPractice/Program.fs
@@ -1,5 +1,6 @@
 ﻿open System
 open FavouriteProgrammingLanguage
+open CoprimeTraversal
 
 let askAboutFavouriteLangSuperpos () = 
     Console.WriteLine("Какой у вас любимый язык программирования?")
@@ -10,8 +11,30 @@ let askAboutFavouriteLangCarry () =
     let func read transform write = write (transform (read ())) 
     func Console.ReadLine favouriteProgrammingLanguage Console.WriteLine
 
+let testCoprimeTraversal () =
+    let number = 15
+    
+    Console.WriteLine("Сумма взаимнопростых чисел с числом {0}", number)
+    let sum = coprimeTraversal number (+) 0
+    Console.WriteLine(sum)
+
+    Console.WriteLine("Произведение взаимнопростых чисел с числом {0}", number)
+    let mult = coprimeTraversal number (*) 1
+    Console.WriteLine(mult)
+
+    Console.WriteLine("Количество взаимнопростых чисел с числом {0}", number)
+    let count = coprimeTraversal number (fun x y -> x + 1) 0
+    Console.WriteLine(count)
+
+    Console.WriteLine("Минимум из взаимнопростых чисел с числом {0}", number)
+    let min = coprimeTraversal number (fun x y -> if x < y then x else y) number
+    Console.WriteLine(min)
+
+    Console.WriteLine("Максимум из взаимнопростых чисел с числом {0}", number)
+    let max = coprimeTraversal number (fun x y -> if x > y then x else y) 0
+    Console.WriteLine(max)
+
 [<EntryPoint>]
 let main argv =
-    askAboutFavouriteLangSuperpos ()
-    askAboutFavouriteLangCarry ()
+    testCoprimeTraversal ()
     0
